2021a8e7782ca267afefaf467f3b35524f4d6ed1
jual6332@colorado.edu <jual6332@colorado.edu>
2022-04-24 12:22:38 -0400

    Added mustang tab files

---
+++ myApp/src/app/mustangtab/mustangtab.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mustangtab',
+  templateUrl: 'mustangtab.page.html',
+  styleUrls: ['mustangtab.page.scss']
+})
+export class MustangTabPage {
+
+  constructor() {}
+
+}
