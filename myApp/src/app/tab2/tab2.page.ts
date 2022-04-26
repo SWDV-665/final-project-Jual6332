f3c64eaff09e417c7142cb459f04a8b55045a8cd
jual6332@colorado.edu <jual6332@colorado.edu>
2022-04-26 17:16:03 -0400

    Added the alert buttons and popup windows for the 1969 Mustang model data

--- myApp/src/app/tab2/tab2.page.ts
+++ myApp/src/app/tab2/tab2.page.ts
@@ -7,6 +7,51 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+
+  async presentAlertManufacturer() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Information Alert',
+      subHeader: '',
+      message: 'The manufacturer of the mustang is Ford Motor Company.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentAlertAvgCost() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Information Alert',
+      subHeader: '',
+      message: 'The average cost for the 1969 Ford Mustang is $127k.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentAlertEngineSize() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Information Alert',
+      subHeader: '',
+      message: 'The engine size of the 1969 Mustang ranges from 5.0L to 5.7L V8.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 }
