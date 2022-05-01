31149f5853f539f639d6773a1941e1a6c5fa8feb
jual6332@colorado.edu <jual6332@colorado.edu>
2022-05-01 06:15:04 -0400

    Added more visuals to the home page for the web app

--- myApp/src/app/tab2/tab2.page.ts
+++ myApp/src/app/tab2/tab2.page.ts
@@ -6,11 +6,11 @@ import { AlertController } from '@ionic/angular';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
+
 export class Tab2Page {
   constructor(public alertController: AlertController) {}
 
-
-  async presentAlertManufacturer() {
+  async presentAlertManufacturerMustang() {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Information Alert',
@@ -25,7 +25,7 @@ export class Tab2Page {
     console.log('onDidDismiss resolved with role', role);
   }
 
-  async presentAlertAvgCost() {
+  async presentAlertAvgCostMustang() {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Information Alert',
@@ -40,12 +40,58 @@ export class Tab2Page {
     console.log('onDidDismiss resolved with role', role);
   }
 
-  async presentAlertEngineSize() {
+  async presentAlertEngineSizeMustang() {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
       header: 'Information Alert',
       subHeader: '',
       message: 'The engine size of the 1969 Mustang ranges from 5.0L to 5.7L V8.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  // Porsche 911 functions
+  async presentAlertEngineSizePorsche911() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Information Alert',
+      subHeader: '',
+      message: 'The engine size of the 1969 Porsche 911 is 5.7L V8.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentAlertAvgCostPorsche911() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Information Alert',
+      subHeader: '',
+      message: 'The average cost for the Porsche 911 is $141k.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentAlertManufacturerPorsche911() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Information Alert',
+      subHeader: '',
+      message: 'The manufacturer of the mustang is Ford Motor Company.',
       buttons: ['OK']
     });
 
